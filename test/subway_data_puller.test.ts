aed6dfb4ad0270f67571f82b0bef43e0838b503c
James Spillmann <james@spacinsider.com>
2024-05-30 15:33:25 -0400

    Initial commit

---
+++ test/subway_data_puller.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SubwayDataPuller from '../lib/subway_data_puller-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/subway_data_puller-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SubwayDataPuller.SubwayDataPullerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
